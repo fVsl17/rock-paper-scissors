e0d99693cf1b0fed4838290301df854348e6741c
Vasile <florin777312@gmail.com>
2024-11-12 11:38:45 +0200

    Added a play game function
    The play game function plays the game 5 times keeps track of score and announces the winner at the end

--- script.js
+++ script.js
@@ -10,31 +10,53 @@ function getHumanChoice () {
     return choice;
 }
 
-let humanScore, computerScore;
-humanScore = computerScore = 0;
 
 
-function playRound(){
+function playRound(humanScore, computerScore){
     const playerChoice = getHumanChoice().toLowerCase();
     const computerChoice = getComputerChoice();
     if (playerChoice === 'rock' && computerChoice === 'scissors'){
         console.log(`You win! ${playerChoice} beats ${computerChoice}`);
         humanScore++;
         console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+        return 1;
     } else if (playerChoice === 'paper' && computerChoice === 'rock'){
         console.log(`You win! ${playerChoice} beats ${computerChoice}`);
         humanScore++;
         console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+        return 1;
     } else if (playerChoice === 'scissors' && computerChoice === 'paper'){
         console.log(`You win! ${playerChoice} beats ${computerChoice}`);
         humanScore++;
         console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+        return 1;
     } else if (playerChoice === computerChoice){
         console.log(`Draw! You both chose ${playerChoice}`);
         console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+        return 0;
     } else {
         console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
         computerScore++;
         console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+        return -1;
+    }
+}
+
+function playGame(){
+    let times = 5, humanScore = 0, computerScore = 0;
+    while (times) {
+        const decide = playRound(humanScore, computerScore);
+        if (decide === 1)
+            humanScore++;
+        if (decide === -1)
+            computerScore++;
+        times--;
+    }
+    if (humanScore > computerScore) {
+        console.log(`You won this game! Your score: ${humanScore} || Computer score: ${computerScore}`);
+    } else if (computerScore > humanScore){
+        console.log(`You lost this game! Your score: ${humanScore} || Computer score: ${computerScore}`);
+    } else {
+        console.log(`Draw! You both have the same score: ${humanScore}`);
     }
 }
