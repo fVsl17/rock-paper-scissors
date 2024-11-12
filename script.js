d1c7a91a371e782460a7392454d960ba6d283009
Vasile <florin777312@gmail.com>
2024-11-12 11:28:15 +0200

    Added the play round functionality
    The player chooses rock paper or scissors then checks if they win or lose updating the score

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ function getHumanChoice () {
     const choice = prompt("Rock, paper or scissors?");
     return choice;
 }
+
+let humanScore, computerScore;
+humanScore = computerScore = 0;
+
+
+function playRound(){
+    const playerChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    if (playerChoice === 'rock' && computerChoice === 'scissors'){
+        console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+        humanScore++;
+        console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+    } else if (playerChoice === 'paper' && computerChoice === 'rock'){
+        console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+        humanScore++;
+        console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper'){
+        console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+        humanScore++;
+        console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+    } else if (playerChoice === computerChoice){
+        console.log(`Draw! You both chose ${playerChoice}`);
+        console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+        computerScore++;
+        console.log(`Your score: ${humanScore} || Computer score: ${computerScore}`);
+    }
+}
